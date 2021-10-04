1b90dd864a283c7aaba0f797233379c67646ca06
aline <alinne.1904@gmail.com>
2021-10-04 01:01:44 -0300

    feat: add route configuration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Characters } from "./pages/characters";
+import { Home } from "./pages/home";
+import { Comics } from "./pages/comics";
+import { Series } from "./pages/series";
+import { Events } from "./pages/events";
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/characters/:id?">
+          <Characters />
+        </Route>
+        <Route path="/comics/:id?">
+          <Comics />
+        </Route>
+        <Route path="/series/:id?">
+          <Series />
+        </Route>
+        <Route path="/events/:id?">
+          <Events />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
-
-export default App;
