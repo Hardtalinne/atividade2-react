306b5a2df8a4c46a3b6549012d72f8c9c9df2271
aline <alinne.1904@gmail.com>
2021-10-04 21:25:48 -0300

    refactor: format indentation

--- src/services/get-characters.js
+++ src/services/get-characters.js
@@ -1,6 +1,10 @@
-import {api} from "./api";
+import { api } from "./api";
 
 export const getCharacters = async (id, value, offset) => {
-    const { data, status } = await api.get( `characters${id ? `/${id}` : ''}${value ? `?nameStartsWith=${value}&` : '?'}${offset ? `offset=${offset}&` : 'offset=1&'}limit=100`)
-    return status === 200 ? data.data : [];
-}
+  const { data, status } = await api.get(
+    `characters${id ? `/${id}` : ""}${
+      value ? `?nameStartsWith=${value}&` : "?"
+    }${offset ? `offset=${offset}&` : "offset=1&"}limit=100`
+  );
+  return status === 200 ? data.data : [];
+};
--- src/services/get-events.js
+++ src/services/get-events.js
@@ -1,6 +1,9 @@
-import {api} from "./api";
+import { api } from "./api";
 
 export const getEvents = async (id, value, offset) => {
-    const { data, status } = await api.get( `events${id ? `/${id}` : ''}${value ? `?titleStartsWith=${value}&` : '?'}${offset ? `offset=${offset}&` : 'offset=1&'}limit=100`)
-    return status === 200 ? data.data : [];
-}
+  const { data, status } = await api.get(
+    `events${id ? `/${id}` : ""}${value ? `?titleStartsWith=${value}&` : "?"}${
+      offset ? `offset=${offset}&` : "offset=1&"}limit=100`
+  );
+  return status === 200 ? data.data : [];
+};
